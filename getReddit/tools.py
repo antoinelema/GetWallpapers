08871a39575eaf149fa1c4a3122b0cd4e81688a6
Unknown <antoinelemariey@gmail.com>
2018-11-01 12:48:06 +0100

    get the wallpaper and set it

--- getReddit/tools.py
+++ getReddit/tools.py
@@ -1,7 +1,10 @@
 import os
 import requests
 import requests.auth
-
+from subprocess import call
+import urllib
+from datetime import datetime
+import sys
 
 def getToken():
     client_auth = requests.auth.HTTPBasicAuth(os.environ['clientID'], os.environ['clientSecret'])
@@ -19,3 +22,33 @@ def getToken():
     json_data = response.json()
     token = json_data['access_token']
     return token
+
+
+def get_all(url,token):
+    """Conection et Get du site."""
+    querystring = {
+        "access_token": token
+        }
+    header = {
+        'User-Agent': 'My User Agent GetRedditWallpaper',
+        'From': 'neiho'
+        }
+
+    return requests.get(url, headers=header, params=querystring)
+
+
+def ext(url):
+    """ trouve et renvoi l'extention d'une url """
+    filename, file_extension = os.path.splitext(url)
+    return(file_extension)
+
+
+def changefond(trueUrlImg):
+    """ enregistre l'image et la met en fond """
+    now=datetime.now()
+    extension=ext(trueUrlImg)
+    pathToImg = os.path.expanduser("~")+"/Images/fonds"
+    cheminImg = "{}/fond.{}-{}-{}{}".format(pathToImg,now.day,now.month,now.year,extension)
+    print("telechargement ...")
+    urllib.request.urlretrieve(trueUrlImg, cheminImg)
+    call(["gsettings", "set", "org.gnome.desktop.background", "picture-uri", "file://{}".format(cheminImg)])  # change le fond d'ecran de l'ordi #gnome3
--- getRedditWallpaper.py
+++ getRedditWallpaper.py
@@ -1,4 +1,13 @@
 from getReddit import tools
+import json
+import os
 
 token = tools.getToken()
-print(token)
+
+url = "https://www.reddit.com/r/wallpapers/hot.json?limit=1"
+
+response = tools.get_all(url, token)
+data = response.json()
+url_img = data['data']["children"][0]['data']['url']
+
+tools.changefond(url_img)
